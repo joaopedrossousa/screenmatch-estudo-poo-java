3caec6f425546ca347c6f26639404228f37614ff
joaopedrossousa <jpsousa.ti@gmail.com>
2025-10-20 15:03:41 -0300

    #

--- src/br/com/alura/screenmatch/models/Movie.java
+++ src/br/com/alura/screenmatch/models/Movie.java
@@ -6,11 +6,16 @@ public class Movie {
     private int countRating;
     int movieDuration;
 
+    int getCountRating() {
+        return countRating;
+    }
+
     void showDataSheet(){
         System.out.println("Nome do filme: " +movieName);
         System.out.println("Ano de Lançamento: " +releaseYear);
         System.out.println("Duração do Filme: " +movieDuration+ " minutos.");
         System.out.println("Avaliação Média: %.1f".formatted(averageRating()));
+        System.out.println("Total de avaliações: " + getCountRating());
     }
 
     void rate(double score){
